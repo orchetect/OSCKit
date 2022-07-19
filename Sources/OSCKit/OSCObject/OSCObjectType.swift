b549090bf67e349b5763b2bec3d0c278d77c6a34
Steffan Andrews <stef@orchetect.com>
2022-07-18 23:25:24 -0700

    Split `OSCObject` into multiple files

---
+++ Sources/OSCKit/OSCObject/OSCObjectType.swift
@@ -0,0 +1,12 @@
+//
+//  OSCObjectType.swift
+//  OSCKit • https://github.com/orchetect/OSCKit
+//
+
+/// Enum describing an OSC message type.
+public enum OSCObjectType: Equatable, Hashable {
+    
+    case message
+    case bundle
+    
+}
