efa747f46b60323a04446e6ea9a843310fe9df0b
Steffan Andrews <stef@orchetect.com>
2022-07-20 01:32:06 -0700

    Formatting

--- Sources/OSCKit/OSCMessageValue/OSCMessageValue Sequence Extensions.swift
+++ Sources/OSCKit/OSCMessageValue/OSCMessageValue Sequence Extensions.swift
@@ -8,7 +8,8 @@ import Foundation
 public extension Sequence where Iterator.Element == OSCMessageValue {
     
     /// Convenience: maps a sequence of `OSCMessageValue`s to a flat string, for logging/debug purposes.
-    func mapDebugString(withLabel: Bool = true, separator: String = ", ") -> String {
+    func mapDebugString(withLabel: Bool = true,
+                        separator: String = ", ") -> String {
         
         self.map { $0.stringValue(withLabel: withLabel) }
             .joined(separator: separator)
