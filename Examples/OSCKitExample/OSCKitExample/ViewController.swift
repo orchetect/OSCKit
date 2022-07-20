83f2d7fac8fa40d0605ee90b3156fe5bef1fc360
Steffan Andrews <stef@orchetect.com>
2022-07-20 01:34:44 -0700

    Updated OSCKitExample project with simple OSC message value mask demonstration

--- Examples/OSCKitExample/OSCKitExample/ViewController.swift
+++ Examples/OSCKitExample/OSCKitExample/ViewController.swift
@@ -49,7 +49,7 @@ class ViewController: NSViewController {
                 
                 do {
                     guard let oscPayload = try data.parseOSC() else { return }
-                    self?.handleOSCPayload(oscPayload)
+                    try self?.handleOSCPayload(oscPayload)
                     
                 } catch let error as OSCBundle.DecodeError {
                     // handle bundle errors
@@ -83,17 +83,40 @@ class ViewController: NSViewController {
     }
     
     /// Handle incoming OSC data recursively
-    func handleOSCPayload(_ oscPayload: OSCPayload) {
+    func handleOSCPayload(_ oscPayload: OSCPayload) throws {
         
         switch oscPayload {
         case .bundle(let bundle):
             // recursively handle nested bundles and messages
-            bundle.elements.forEach { handleOSCPayload($0) }
+            try bundle.elements.forEach { try handleOSCPayload($0) }
             
         case .message(let message):
             // handle message
-            print(message)
+            try handleOSCMessage(message)
             
+        }
+        
+    }
+    
+    func handleOSCMessage(_ oscMessage: OSCMessage) throws {
+        
+        switch oscMessage.address.pathComponents {
+        case ["test", "method1"]:
+            // validate value array with expected types: [Int32]
+            let value = try oscMessage.values.masked(Int32.self)
+            print("/test/method1 with int32:", value)
+            
+        case ["test", "method2"]:
+            // validate value array with expected types: [Int32, String?]
+            let values = try oscMessage.values.masked(Int32.self, ASCIIString?.self)
+            if let values1 = values.1 {
+                print("/test/method2 with int32: \(values.0), string: \(values1)")
+            } else {
+                print("/test/method2 with int32: \(values.0)")
+            }
+            
+        default:
+            print(oscMessage)
         }
         
     }
