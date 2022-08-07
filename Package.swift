757eb1a7cf537fcf9b8178c973a08b22157367b5
Steffan Andrews <stef@orchetect.com>
2022-08-06 19:15:23 -0700

    Added OSCKitTests test target

--- Package.swift
+++ Package.swift
@@ -4,7 +4,6 @@
 import PackageDescription
 
 let package = Package(
-    
     name: "OSCKit",
     
     platforms: [.macOS(.v10_12), .iOS(.v10), .tvOS(.v10)],
@@ -12,52 +11,67 @@ let package = Package(
     products: [
         .library(
             name: "OSCKit",
-            targets: ["OSCKit"]),
+            targets: ["OSCKit"]
+        ),
         .library(
             name: "OSCKitCore",
-            targets: ["OSCKitCore"])
+            targets: ["OSCKitCore"]
+        )
     ],
     
     dependencies: [
         .package(url: "https://github.com/orchetect/OTCore", from: "1.4.1"),
         .package(url: "https://github.com/orchetect/SwiftASCII", from: "1.1.3"),
         .package(url: "https://github.com/orchetect/SwiftRadix", from: "1.2.0"),
-        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket", from: "7.0.0"),
+        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket", from: "7.0.0")
     ],
     
     targets: [
         .target(
             name: "OSCKit",
-            dependencies: ["OSCKitCore", "CocoaAsyncSocket"]),
+            dependencies: ["OSCKitCore", "CocoaAsyncSocket"]
+        ),
         .target(
             name: "OSCKitCore",
-            dependencies: ["OTCore", "SwiftASCII", "SwiftRadix"]),
+            dependencies: ["OTCore", "SwiftASCII", "SwiftRadix"]
+        ),
+        
+        // Test targets
+        .testTarget(
+            name: "OSCKitTests",
+            dependencies: ["OSCKit", "OTCore"]
+        ),
         .testTarget(
             name: "OSCKitCoreTests",
-            dependencies: ["OSCKitCore", "OTCore"]),
+            dependencies: ["OSCKitCore", "OTCore"]
+        )
     ]
-    
 )
 
-func addShouldTestFlag() {
+func addShouldTestFlag(toTarget targetName: String) {
     // swiftSettings may be nil so we can't directly append to it
     
     var swiftSettings = package.targets
-        .first(where: { $0.name == "OSCKitCoreTests" })?
+        .first(where: { $0.name == targetName })?
         .swiftSettings ?? []
     
     swiftSettings.append(.define("shouldTestCurrentPlatform"))
     
     package.targets
-        .first(where: { $0.name == "OSCKitCoreTests" })?
+        .first(where: { $0.name == targetName })?
         .swiftSettings = swiftSettings
+}
+
+func addShouldTestFlags() {
+    addShouldTestFlag(toTarget: "OSCKitTests")
+    addShouldTestFlag(toTarget: "OSCKitCoreTests")
 }
 
 // Swift version in Xcode 12.5.1 which introduced watchOS testing
 #if os(watchOS) && swift(>=5.4.2)
-addShouldTestFlag()
+addShouldTestFlags()
 #elseif os(watchOS)
 // don't add flag
 #else
-addShouldTestFlag()
+addShouldTestFlags()
 #endif
