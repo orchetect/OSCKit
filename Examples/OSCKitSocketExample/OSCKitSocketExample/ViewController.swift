e9327e7ca1f0c9849f7f212b449032e282784998
Steffan Andrews <stef@orchetect.com>
2023-04-07 22:59:44 -0700

    Renamed example project

--- Examples/OSCKitSocketExample/OSCKitSocketExample/ViewController.swift
+++ Examples/OSCKitSocketExample/OSCKitSocketExample/ViewController.swift
@@ -1,68 +1,10 @@
 //
-//  AppDelegate.swift
+//  ViewController.swift
 //  OSCKit • https://github.com/orchetect/OSCKit
 //  © 2020-2023 Steffan Andrews • Licensed under MIT License
 //
 
 import Cocoa
-import OSCKit
-
-@main
-class AppDelegate: NSObject, NSApplicationDelegate {
-    var oscPeer: OSCSocket?
-    var localPort: UInt16?
-    var remoteHost: String = ""
-    var remotePort: UInt16?
-    var isPortReuseEnabled: Bool = false
-    var isIPv4BroadcastEnabled: Bool = false
-    
-    func applicationDidFinishLaunching(_ aNotification: Notification) { }
-    
-    func applicationWillTerminate(_ aNotification: Notification) {
-        oscPeer?.stop()
-    }
-    
-    @IBAction
-    func startOSCPeer(_ sender: Any?) {
-        print("Starting OSC socket.")
-        do {
-            let newPeer = OSCSocket(
-                localPort: localPort,
-                remoteHost: remoteHost,
-                remotePort: remotePort
-            ) { message, timeTag in
-                print(message, "with time tag: \(timeTag)")
-            }
-            oscPeer = newPeer
-            
-            newPeer.isPortReuseEnabled = isPortReuseEnabled
-            newPeer.isIPv4BroadcastEnabled = isIPv4BroadcastEnabled
-            
-            try newPeer.start()
-            print("Using local port \(newPeer.localPort) and remote port \(newPeer.remotePort) with remote host \(remoteHost).")
-        } catch {
-            print("Error while starting OSC socket: \(error)")
-        }
-    }
-    
-    @IBAction
-    func stopOSCPeer(_ sender: Any?) {
-        print("Stopping OSC socket.")
-        oscPeer?.stop()
-    }
-    
-    /// Send a test OSC message.
-    @IBAction
-    func sendTestOSCMessage(_ sender: Any?) {
-        do {
-            try oscPeer?.send(
-                .message("/conduit/Status", values: ["System"])
-            )
-        } catch {
-            print("Error while sending: \(error)")
-        }
-    }
-}
 
 class ViewController: NSViewController {
     @IBOutlet var localPortNumber: NSTextField!
@@ -85,12 +27,12 @@ class ViewController: NSViewController {
         appDelegate?.remoteHost = remoteHost.stringValue
         
         appDelegate?.localPort = !localPortNumber.stringValue.isEmpty
-            ? UInt16(localPortNumber.stringValue)
-            : nil
+        ? UInt16(localPortNumber.stringValue)
+        : nil
         
         appDelegate?.remotePort = !remotePortNumber.stringValue.isEmpty
-            ? UInt16(remotePortNumber.stringValue)
-            : nil
+        ? UInt16(remotePortNumber.stringValue)
+        : nil
         
         appDelegate?.isPortReuseEnabled = enablePortReuse.state == .on
         
