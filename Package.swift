12e8b16011d2bc79e22b5e675ea0bd9b81b97577
Steffan Andrews <stef@orchetect.com>
2025-04-08 16:54:13 -0700

    Removed watchOS from package manifest platforms

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "OSCKit",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13)],
     products: [
         .library(
             name: "OSCKit",
