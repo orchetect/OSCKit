52a7c048089e497d626a827888c4bfd295666035
Steffan Andrews <stef@orchetect.com>
2025-07-25 00:04:15 -0700

    `OSCUDPServerDelegate`: Cleanup

--- Sources/OSCKit/UDP/Server/OSCUDPServerDelegate.swift
+++ Sources/OSCKit/UDP/Server/OSCUDPServerDelegate.swift
@@ -11,9 +11,9 @@ import Foundation
 
 /// Internal UDP receiver class so as to not expose `GCDAsyncUdpSocketDelegate` methods as public.
 final class OSCUDPServerDelegate: NSObject {
-    weak var oscServer: _OSCHandlerProtocol?
+    weak var oscServer: (any _OSCHandlerProtocol)?
     
-    init(oscServer: _OSCHandlerProtocol? = nil) {
+    init(oscServer: (any _OSCHandlerProtocol)? = nil) {
         self.oscServer = oscServer
     }
 }
@@ -42,20 +42,18 @@ extension OSCUDPServerDelegate: GCDAsyncUdpSocketDelegate {
     
     /// Stub required to take `oscServer` as sending.
     private func _handle(
-        oscServer: _OSCHandlerProtocol,
+        oscServer: any _OSCHandlerProtocol,
         data: Data,
         remoteHost: String,
         remotePort: UInt16
     ) {
-        oscServer.queue.async {
-            do {
-                guard let payload = try data.parseOSC() else { return }
-                oscServer._handle(payload: payload, remoteHost: remoteHost, remotePort: remotePort)
-            } catch {
-                #if DEBUG
-                print("OSC parse error: \(error.localizedDescription)")
-                #endif
-            }
+        do {
+            guard let payload = try data.parseOSC() else { return }
+            oscServer._handle(payload: payload, remoteHost: remoteHost, remotePort: remotePort)
+        } catch {
+            #if DEBUG
+            print("OSC parse error: \(error.localizedDescription)")
+            #endif
         }
     }
 }
