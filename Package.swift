f581fc1964200b5e94f0813295cfbb08eba7d040
Steffan Andrews <stef@orchetect.com>
2025-09-15 14:57:21 -0700

    Bumped dependency versions

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/orchetect/SwiftASCII", from: "1.2.0"),
-        .package(url: "https://github.com/apple/swift-numerics", from: "1.0.2")
+        .package(url: "https://github.com/apple/swift-numerics", from: "1.1.0")
     ],
     targets: [
         .target(
