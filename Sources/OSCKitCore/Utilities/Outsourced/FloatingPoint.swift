d222f3e40a3152478bc4af59656671ee31834aea
Steffan Andrews <stef@orchetect.com>
2026-04-04 15:32:39 -0700

    Replaced usage of `trunc()` with Swift-native `rounded(.towardZero)` (#63)

--- Sources/OSCKitCore/Utilities/Outsourced/FloatingPoint.swift
+++ Sources/OSCKitCore/Utilities/Outsourced/FloatingPoint.swift
@@ -1,25 +1,19 @@
 /// ----------------------------------------------
 /// ----------------------------------------------
-/// OTCore/Extensions/Darwin/FloatingPoint and Darwin.swift
+/// Extensions/Darwin/FloatingPoint.swift
 ///
-/// Borrowed from OTCore 1.4.10 under MIT license.
-/// https://github.com/orchetect/OTCore
+/// Borrowed from swift-extensions 2.1.6 under MIT license.
+/// https://github.com/orchetect/swift-extensions
 /// Methods herein are unit tested at their source
 /// so no unit tests are necessary.
 /// ----------------------------------------------
 /// ----------------------------------------------
 
-#if canImport(Darwin)
-import Darwin
-#elseif canImport(Glibc)
-import Glibc
-#endif
-
 extension FloatingPoint {
     /// Similar to `Int.quotientAndRemainder(dividingBy:)` from the standard Swift library.
     package func quotientAndRemainder(dividingBy rhs: Self) -> (quotient: Self, remainder: Self) {
         let calculation = self / rhs
-        let integral = trunc(calculation)
+        let integral = calculation.rounded(.towardZero)
         let fraction = self - (integral * rhs)
         return (quotient: integral, remainder: fraction)
     }
@@ -32,7 +26,7 @@ extension FloatingPoint {
     /// This method can result in a non-trivial loss of precision for the fractional part.
     @inlinable
     package var integralAndFraction: (integral: Self, fraction: Self) {
-        let integral = trunc(self)
+        let integral = rounded(.towardZero)
         let fraction = self - integral
         return (integral: integral, fraction: fraction)
     }
